36fafa63880bef60974ba39d2725e252813a456a
HeYu <code@qheyu.com>
2025-07-31 18:57:00 +0800

    refactor: 移除未用到的工具函数等代码

--- internal/utils/strkit/case.go
+++ internal/utils/strkit/case.go
@@ -13,30 +13,6 @@ func UpperFirst(str string) string {
 	}
 }
 
-func LowerFirst(str string) string {
-	switch len(str) {
-	case 0:
-		return ""
-	case 1:
-		return strings.ToLower(str)
-	default:
-		return strings.ToLower(str[0:1]) + str[1:]
-	}
-}
-
-func CamelCase(str string) string {
-	parts := strings.Split(str, "_")
-	ucParts := make([]string, len(parts))
-	for i, part := range parts {
-		if i == 0 {
-			ucParts[i] = part
-		} else {
-			ucParts[i] = UpperFirst(part)
-		}
-	}
-	return strings.Join(ucParts, "")
-}
-
 func UpperCamelCase(str string) string {
 	parts := strings.Split(str, "_")
 	ucParts := make([]string, len(parts))
